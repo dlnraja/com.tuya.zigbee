ea9a094c2969c5fc3280cfde4553edf10c4ec80a
dlnraja <dylan.rajasekaram+homey@gmail.com>
2025-07-27 05:25:55 +0200

    RESTAURATION CONTEXTE - 2025-07-27 05:25:54 - Version 1.0.25 - Contexte logique restauré après déconnexion, toutes les tâches reprises automatiquement

--- zigbee-cluster-referential-system.js
+++ zigbee-cluster-referential-system.js
@@ -318,12 +318,7 @@ class ZigbeeClusterReferential {
                         },
                         {
                             name: "Commit and Push",
-                            run: |
-                                git config --local user.email "action@github.com"
-                                git config --local user.name "GitHub Action"
-                                git add .
-                                git commit -m "Monthly Zigbee Referential Update - $(date)"
-                                git push
+                            run: "git config --local user.email 'action@github.com' && git config --local user.name 'GitHub Action' && git add . && git commit -m 'Monthly Zigbee Referential Update' && git push"
                         }
                     ]
                 }
