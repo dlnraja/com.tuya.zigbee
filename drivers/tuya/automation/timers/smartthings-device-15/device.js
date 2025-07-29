5791f8f34b3917dff3d1d393fb49c675f2d1fe61
dlnraja <dylan.rajasekaram+homey@gmail.com>
2025-07-29 22:59:17 +0200

    🔄 feat: Script de synchronisation automatique tuya-light branch

---
+++ drivers/tuya/automation/timers/smartthings-device-15/device.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const { TuyaDevice } = require('homey-tuya');
+const { TuyaZigbeeDevice } = require('homey-tuya-zigbee');
+
+class Smartthingsdevice15Device extends TuyaDevice {
+    
+    async onInit() {
+        try {
+        await super.onInit();
+        
+        // Initialize device
+        this.log('smartthings-device-15 device initialized');
+        
+        // Register capabilities
+        this.registerCapability('onoff', true);
+        } catch (error) {
+            this.log('Erreur initialisation:', error);
+        }
+        this.registerCapability('dim', true);
+        this.registerCapability('measure_power', true);
+        
+        // Register flows
+        this.registerFlowCards();
+    }
+    
+    async onUninit() {
+        await super.onUninit();
+        this.log('smartthings-device-15 device uninitialized');
+    }
+    
+    registerFlowCards() {
+        // Register flow cards if needed
+    }
+    
+    async onSettings({ oldSettings, newSettings, changedKeys }) {
+        await super.onSettings({ oldSettings, newSettings, changedKeys });
+        this.log('smartthings-device-15 settings updated');
+    }
+}
+
+module.exports = Smartthingsdevice15Device;
