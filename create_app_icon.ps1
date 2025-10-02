ee6261a9c437e0901cea081d2a7219ef60686bac
dlnraja <dylan.rajasekaram@gmail.com>
2025-10-02 23:56:50 +0200

    Organization: move old files to archive/ & normalize manufacturers

--- create_app_icon.ps1
+++
@@ -1,8 +0,0 @@
-Add-Type -AssemblyName System.Drawing
-$bitmap = New-Object System.Drawing.Bitmap(75, 75)
-$graphics = [System.Drawing.Graphics]::FromImage($bitmap)
-$graphics.Clear([System.Drawing.Color]::FromArgb(255, 230, 230, 230))
-$graphics.Dispose()
-$bitmap.Save('assets\small.png', [System.Drawing.Imaging.ImageFormat]::Png)
-$bitmap.Dispose()
-Write-Host 'Created 75x75 image for assets\small.png'
