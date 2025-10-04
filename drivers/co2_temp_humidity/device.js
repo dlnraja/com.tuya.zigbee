7c453307f6deb5f5c02e0560c12ddb9cd63d3536
dlnraja <dylan.rajasekaram@gmail.com>
2025-10-05 00:50:22 +0200

    V37.0 Deep Modification: auto-fix manifests via orchestrator, BDU v37 generated

--- drivers/co2_temp_humidity/device.js
+++ drivers/co2_temp_humidity/device.js
@@ -4,8 +4,46 @@ const { ZigBeeDevice } = require('homey-zigbeedriver');
 class CO2Device extends ZigBeeDevice {
   async onNodeInit() {
     this.enableDebug();
-    this.registerCapability('measure_co2', 'CLUSTER_TUYA_SPECIFIC');
-    this.registerCapability('measure_temperature', 'CLUSTER_TUYA_SPECIFIC');
+    this.printNode();
+    // Register capabilities with numeric Zigbee clusters
+    if (this.hasCapability('onoff')) {
+      this.registerCapability('onoff', 6);
+    }
+    if (this.hasCapability('dim')) {
+      this.registerCapability('dim', 8);
+    }
+    if (this.hasCapability('light_hue')) {
+      this.registerCapability('light_hue', 768);
+    }
+    if (this.hasCapability('light_saturation')) {
+      this.registerCapability('light_saturation', 768);
+    }
+    if (this.hasCapability('light_temperature')) {
+      this.registerCapability('light_temperature', 768);
+    }
+    if (this.hasCapability('measure_temperature')) {
+      this.registerCapability('measure_temperature', 1026);
+    }
+    if (this.hasCapability('measure_humidity')) {
+      this.registerCapability('measure_humidity', 1029);
+    }
+    if (this.hasCapability('alarm_motion')) {
+      this.registerCapability('alarm_motion', 1280);
+    }
+    if (this.hasCapability('alarm_contact')) {
+      this.registerCapability('alarm_contact', 1280);
+    }
+    if (this.hasCapability('alarm_co')) {
+      this.registerCapability('alarm_co', 1280);
+    }
+    if (this.hasCapability('measure_battery')) {
+      this.registerCapability('measure_battery', 1);
+    }
+    if (this.hasCapability('alarm_battery')) {
+      this.registerCapability('alarm_battery', 1);
+    }
+    // replaced by numeric cluster registration
+    // replaced by numeric cluster registration
   }
 }
 module.exports = CO2Device;
