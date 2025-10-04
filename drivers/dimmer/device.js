02eb95bb083b5fc87e6554e400d4ecb2d7db34d5
dlnraja <dylan.rajasekaram@gmail.com>
2025-10-05 00:31:48 +0200

    V37.0 Deep Modification: auto-fix manifests via orchestrator, BDU v37 generated

--- drivers/dimmer/device.js
+++ drivers/dimmer/device.js
@@ -5,30 +5,15 @@ const { ZigBeeDevice } = require('homey-zigbeedriver');
 class DimmerDevice extends ZigBeeDevice {
 
     async onNodeInit() {
-        this.log('dimmer device initialized');
+        this.enableDebug();
+        this.printNode();
 
-        // Register capabilities
-                // Register on/off capability
-        this.registerCapabilityListener('onoff', this.onCapabilityOnoff.bind(this));
-
-        // Mark device as available
-        await this.setAvailable();
-    }
-
-        async onCapabilityOnoff(value, opts) {
-        this.log('onCapabilityOnoff:', value);
-        
-        try {
-            if (value) {
-                await this.zclNode.endpoints[1].clusters.onOff.setOn();
-            } else {
-                await this.zclNode.endpoints[1].clusters.onOff.setOff();
-            }
-            
-            return Promise.resolve();
-        } catch (error) {
-            this.error('Error setting onoff:', error);
-            return Promise.reject(error);
+        // Register capabilities with numeric clusters
+        if (this.hasCapability('onoff')) {
+            this.registerCapability('onoff', 6);
+        }
+        if (this.hasCapability('dim')) {
+            this.registerCapability('dim', 8);
         }
     }
 
