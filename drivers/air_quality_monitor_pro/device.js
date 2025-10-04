92c36e47801a649b33f40f5e7ae0d600e68d8829
dlnraja <dylan.rajasekaram@gmail.com>
2025-10-05 00:45:52 +0200

    V37.0 Deep Modification: auto-fix manifests via orchestrator, BDU v37 generated

--- drivers/air_quality_monitor_pro/device.js
+++ drivers/air_quality_monitor_pro/device.js
@@ -7,10 +7,43 @@ class TuyaDevice extends ZigBeeDevice {
     this.printNode();
     
     // Register capabilities
-    const caps = this.getCapabilities();
-    caps.forEach(cap => {
-      this.registerCapability(cap, 'CLUSTER_TUYA_SPECIFIC');
-    });
+    // Register capabilities with numeric Zigbee clusters
+    if (this.hasCapability('onoff')) {
+      this.registerCapability('onoff', 6);
+    }
+    if (this.hasCapability('dim')) {
+      this.registerCapability('dim', 8);
+    }
+    if (this.hasCapability('light_hue')) {
+      this.registerCapability('light_hue', 768);
+    }
+    if (this.hasCapability('light_saturation')) {
+      this.registerCapability('light_saturation', 768);
+    }
+    if (this.hasCapability('light_temperature')) {
+      this.registerCapability('light_temperature', 768);
+    }
+    if (this.hasCapability('measure_temperature')) {
+      this.registerCapability('measure_temperature', 1026);
+    }
+    if (this.hasCapability('measure_humidity')) {
+      this.registerCapability('measure_humidity', 1029);
+    }
+    if (this.hasCapability('alarm_motion')) {
+      this.registerCapability('alarm_motion', 1280);
+    }
+    if (this.hasCapability('alarm_contact')) {
+      this.registerCapability('alarm_contact', 1280);
+    }
+    if (this.hasCapability('alarm_co')) {
+      this.registerCapability('alarm_co', 1280);
+    }
+    if (this.hasCapability('measure_battery')) {
+      this.registerCapability('measure_battery', 1);
+    }
+    if (this.hasCapability('alarm_battery')) {
+      this.registerCapability('alarm_battery', 1);
+    }
   }
 }
 module.exports = TuyaDevice;
