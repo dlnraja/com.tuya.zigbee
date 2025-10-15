03c1953eb762fbd3e944fcc65469e88d839451a7
dlnraja <dylan.rajasekaram@gmail.com>
2025-10-15 16:01:47 +0200

    feat: Auto-organize reports - Clean root directory

--- commit_and_push.bat
+++ commit_and_push.bat
@@ -1,5 +1,6 @@
 @echo off
 cd /d "c:\Users\HP\Desktop\homey app\tuya_repair"
 git add -A
-git commit -m "feat: Add git commit helpers and orchestration report"
+git commit -m "feat: Auto-organize reports - Clean root directory"
 git push origin master
+del "%~f0"
