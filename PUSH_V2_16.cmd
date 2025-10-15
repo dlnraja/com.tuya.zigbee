82986e12167326e206848154edc02ad263ecae2c
dlnraja <dylan.rajasekaram@gmail.com>
2025-10-15 21:31:16 +0200

    fix: v2.16.0 - All validations PASS - Ready to publish

---
+++ PUSH_V2_16.cmd
@@ -0,0 +1,2 @@
+git commit -m "feat: v2.16.0 - Fix flow warnings + Ready for first publish"
+git push origin master
---
+++ VERSION_2.16.0.cmd
@@ -0,0 +1,2 @@
+git commit -m "feat: v2.16.0 - Enrich flows with titleFormatted - Ready for publish"
+git push origin master
