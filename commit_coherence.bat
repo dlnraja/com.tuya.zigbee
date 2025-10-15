eba7eefd83736841d257141e5fc0c82ef033b044
dlnraja <dylan.rajasekaram@gmail.com>
2025-10-15 16:13:18 +0200

    feat: Project coherence checker and syntax fixes

--- commit_coherence.bat
+++ commit_coherence.bat
@@ -1,7 +1,6 @@
 @echo off
 cd /d "c:\Users\HP\Desktop\homey app\tuya_repair"
 git add -A
-git commit -m "chore: Update gitignore rules"
-git pull origin master
+git commit -m "feat: Project coherence checker and syntax fixes"
 git push origin master
 del "%~f0"
