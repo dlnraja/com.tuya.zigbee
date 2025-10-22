f8e2c91abcd137809360d7d42c42d2007ba205c7
Dylan Rajasekaram <dylan.rajasekaram@gmail.com>
2025-10-23 01:51:43 +0200

    v4.2.0 FINAL - Eliminated ALL 165 energy-suffix drivers, 186 clean unified drivers, ZERO suffixes remaining

--- drivers/avatto_co2_temp_humidity_cr2032/driver.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const { ZigBeeDriver } = require('homey-zigbeedriver');
-
-class CO2Driver extends ZigBeeDriver {
-  onInit() {
-    this.log('CO2 Driver initialized');
-  }
-}
-module.exports = CO2Driver;
