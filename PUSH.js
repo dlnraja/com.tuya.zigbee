26db97a28cbe9d1082147e9928031a853b049afb
Dylan Rajasekaram <dylan.rajasekaram@gmail.com>
2025-10-23 03:58:23 +0200

    v4.2.2 - Advanced image validation scripts + 15 learnmode.svg

---
+++ PUSH.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+/**
+ * QUICK PUSH - Ultra simple wrapper
+ * 
+ * Usage: node PUSH.js
+ * 
+ * This is the simplest way to push and publish!
+ */
+
+console.log('🚀 Starting SAFE PUSH AND PUBLISH...\n');
+
+require('./scripts/deployment/SAFE_PUSH_AND_PUBLISH.js');
