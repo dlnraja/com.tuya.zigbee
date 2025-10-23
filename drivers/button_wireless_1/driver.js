78ca022d0fdc2b803e3114977e4715a52882eb96
Dylan Rajasekaram <dylan.rajasekaram@gmail.com>
2025-10-23 17:06:28 +0200

    v4.5.0_complete_unbranded_migration

---
+++ drivers/button_wireless_1/driver.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const { ZigBeeDriver } = require('homey-zigbeedriver');
+
+class Button1GangDriver extends ZigBeeDriver {
+
+  async onInit() {
+    this.log('Button1GangDriver initialized');
+    
+    // Register universal flow cards (shared across all button drivers)
+    this.registerUniversalFlowCards();
+  }
+
+  /**
+   * Register universal button flow cards
+   * These work for any button device
+   */
+  registerUniversalFlowCards() {
+    // Generic button pressed (any button)
+    this.homey.flow.getDeviceTriggerCard('button_wireless_1_button_pressed')
+      .registerRunListener(async (args, state) => {
+        return true; // Always trigger
+      });
+
+    // Generic double press
+    this.homey.flow.getDeviceTriggerCard('button_wireless_1_button_double_press')
+      .registerRunListener(async (args, state) => {
+        return true;
+      });
+
+    // Generic long press
+    this.homey.flow.getDeviceTriggerCard('button_wireless_1_button_long_press')
+      .registerRunListener(async (args, state) => {
+        return true;
+      });
+
+    // Generic multi press
+    this.homey.flow.getDeviceTriggerCard('button_wireless_1_button_multi_press')
+      .registerRunListener(async (args, state) => {
+        return true;
+      });
+  }
+}
+
+module.exports = Button1GangDriver;
