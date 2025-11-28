e96d0bd147c4836813836b96292948fe67fe18e3
Dylan Rajasekaram <dylan.rajasekaram@gmail.com>
2025-11-28 23:21:35 +0100

    v5.2.27: MAJOR CONSOLIDATION - 152 drivers merged into 60 clean categories

--- drivers/heating_controller_trv/driver.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-const { ZigBeeDriver } = require('homey-zigbeedriver');
-class Driver extends ZigBeeDriver { onInit() { this.log('heating_controller_trv driver initialized'); } }
-module.exports = Driver;
--- drivers/wall_touch_5gang/README.md
+++
@@ -1,4 +0,0 @@
-# Wall Touch Button 5 Gang
-
-Hybrid Wall Touch Switch - 5 Gang
-Supports both battery and AC power operation.
