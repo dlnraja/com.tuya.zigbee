caaaaf150284cf37dbf694571a3e243c8d14f4fe
Dylan Rajasekaram <dylan.rajasekaram@gmail.com>
2025-11-28 21:09:29 +0100

    v5.2.19: Add environmental sensors (Netatmo alternatives) - Weather Station, CO2/Air Quality, Rain, Formaldehyde, Noise sensors

---
+++ drivers/noise_sensor/driver.js
@@ -0,0 +1,4 @@
+'use strict';
+const { ZigBeeDriver } = require('homey-zigbeedriver');
+class Driver extends ZigBeeDriver { onInit() { this.log('noise_sensor driver init'); } }
+module.exports = Driver;
