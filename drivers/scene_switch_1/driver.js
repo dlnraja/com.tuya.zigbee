b346f2060347429fcd0ee0d43812ff06c3cc2ea9
Dylan Rajasekaram <dylan.rajasekaram@gmail.com>
2026-01-20 08:14:19 +0100

    v5.5.698: Scene switch flow cards - Added flow triggers for scene_switch_1/2/3/6 drivers

--- drivers/scene_switch_1/driver.js
+++ drivers/scene_switch_1/driver.js
@@ -6,6 +6,30 @@ class SceneSwitch1Driver extends ZigBeeDriver {
 
   async onInit() {
     this.log('SceneSwitch1Driver initialized');
+
+    // Register flow triggers
+    this._buttonPressedTrigger = this.homey.flow.getDeviceTriggerCard('scene_switch_1_button_pressed');
+    this._buttonDoubleTrigger = this.homey.flow.getDeviceTriggerCard('scene_switch_1_button_double_press');
+    this._buttonLongTrigger = this.homey.flow.getDeviceTriggerCard('scene_switch_1_button_long_press');
+    this._batteryChangedTrigger = this.homey.flow.getDeviceTriggerCard('scene_switch_1_battery_changed');
+
+    this.log('SceneSwitch1Driver flow triggers registered');
+  }
+
+  triggerButtonPressed(device) {
+    return this._buttonPressedTrigger.trigger(device, {}).catch(this.error);
+  }
+
+  triggerButtonDoublePressed(device) {
+    return this._buttonDoubleTrigger.trigger(device, {}).catch(this.error);
+  }
+
+  triggerButtonLongPressed(device) {
+    return this._buttonLongTrigger.trigger(device, {}).catch(this.error);
+  }
+
+  triggerBatteryChanged(device, tokens) {
+    return this._batteryChangedTrigger.trigger(device, tokens).catch(this.error);
   }
 
 }
