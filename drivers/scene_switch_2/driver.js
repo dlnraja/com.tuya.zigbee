a36eccb9359a8168734db2926e5d8b2533bbfc76
Dylan Rajasekaram <dylan.rajasekaram@gmail.com>
2026-01-20 08:14:19 +0100

    v5.5.698: Scene switch flow cards - Added flow triggers for scene_switch_1/2/3/6 drivers

--- drivers/scene_switch_2/driver.js
+++ drivers/scene_switch_2/driver.js
@@ -6,6 +6,30 @@ class SceneSwitch2Driver extends ZigBeeDriver {
 
   async onInit() {
     this.log('SceneSwitch2Driver initialized');
+
+    // Register flow triggers with button argument
+    this._buttonPressedTrigger = this.homey.flow.getDeviceTriggerCard('scene_switch_2_button_pressed');
+    this._buttonDoubleTrigger = this.homey.flow.getDeviceTriggerCard('scene_switch_2_button_double_press');
+    this._buttonLongTrigger = this.homey.flow.getDeviceTriggerCard('scene_switch_2_button_long_press');
+
+    // Register argument filters
+    this._buttonPressedTrigger.registerRunListener(async (args, state) => args.button === state.button);
+    this._buttonDoubleTrigger.registerRunListener(async (args, state) => args.button === state.button);
+    this._buttonLongTrigger.registerRunListener(async (args, state) => args.button === state.button);
+
+    this.log('SceneSwitch2Driver flow triggers registered');
+  }
+
+  triggerButtonPressed(device, button) {
+    return this._buttonPressedTrigger.trigger(device, { button }, { button }).catch(this.error);
+  }
+
+  triggerButtonDoublePressed(device, button) {
+    return this._buttonDoubleTrigger.trigger(device, { button }, { button }).catch(this.error);
+  }
+
+  triggerButtonLongPressed(device, button) {
+    return this._buttonLongTrigger.trigger(device, { button }, { button }).catch(this.error);
   }
 
 }
--- drivers/scene_switch_3/driver.js
+++ drivers/scene_switch_3/driver.js
@@ -6,6 +6,30 @@ class SceneSwitch3Driver extends ZigBeeDriver {
 
   async onInit() {
     this.log('SceneSwitch3Driver initialized');
+
+    // Register flow triggers with button argument
+    this._buttonPressedTrigger = this.homey.flow.getDeviceTriggerCard('scene_switch_3_button_pressed');
+    this._buttonDoubleTrigger = this.homey.flow.getDeviceTriggerCard('scene_switch_3_button_double_press');
+    this._buttonLongTrigger = this.homey.flow.getDeviceTriggerCard('scene_switch_3_button_long_press');
+
+    // Register argument filters
+    this._buttonPressedTrigger.registerRunListener(async (args, state) => args.button === state.button);
+    this._buttonDoubleTrigger.registerRunListener(async (args, state) => args.button === state.button);
+    this._buttonLongTrigger.registerRunListener(async (args, state) => args.button === state.button);
+
+    this.log('SceneSwitch3Driver flow triggers registered');
+  }
+
+  triggerButtonPressed(device, button) {
+    return this._buttonPressedTrigger.trigger(device, { button }, { button }).catch(this.error);
+  }
+
+  triggerButtonDoublePressed(device, button) {
+    return this._buttonDoubleTrigger.trigger(device, { button }, { button }).catch(this.error);
+  }
+
+  triggerButtonLongPressed(device, button) {
+    return this._buttonLongTrigger.trigger(device, { button }, { button }).catch(this.error);
   }
 
 }
