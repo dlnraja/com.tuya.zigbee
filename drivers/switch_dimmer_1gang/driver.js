592a8d81d0386019570b35e201ee039bb6d39573
Attilla de Groot <attilla@nvidia.com>
2026-01-22 00:30:12 +0100

    next commit

--- drivers/switch_dimmer_1gang/driver.js
+++ drivers/switch_dimmer_1gang/driver.js
@@ -4,16 +4,45 @@ const Homey = require('homey');
 
 class SwitchDimmer1GangDriver extends Homey.Driver {
 
-  /**
-   * onInit is called when the driver is initialized.
-   */
   async onInit() {
-    this.log('Bseed Dimmer 1 Gang Driver has been initialized');
+    this.log('Switch Touch Dimmer (1 Gang) Driver has been initialized');
+    
+    // Register flow card listeners
+    this.registerFlowCards();
   }
 
-  /**
-   * onPairListDevices is called when a user is pairing
-   */
+  registerFlowCards() {
+    // Register trigger cards (already handled in device.js via trigger())
+    this.homey.flow.getDeviceTriggerCard('switch_dimmer_1gang_turned_on');
+    this.homey.flow.getDeviceTriggerCard('switch_dimmer_1gang_turned_off');
+    this.homey.flow.getDeviceTriggerCard('switch_dimmer_1gang_brightness_increased');
+    this.homey.flow.getDeviceTriggerCard('switch_dimmer_1gang_brightness_decreased');
+
+    // Register condition card
+    this.homey.flow.getConditionCard('switch_dimmer_1gang_is_on')
+      .registerRunListener(async (args) => {
+        return args.device.getCapabilityValue('onoff');
+      });
+
+    // Register action cards
+    this.homey.flow.getActionCard('switch_dimmer_1gang_turn_on')
+      .registerRunListener(async (args) => {
+        return args.device.setCapabilityValue('onoff', true);
+      });
+
+    this.homey.flow.getActionCard('switch_dimmer_1gang_turn_off')
+      .registerRunListener(async (args) => {
+        return args.device.setCapabilityValue('onoff', false);
+      });
+
+    this.homey.flow.getActionCard('switch_dimmer_1gang_set_brightness')
+      .registerRunListener(async (args) => {
+        return args.device.setCapabilityValue('dim', args.brightness);
+      });
+
+    this.log('Flow cards registered');
+  }
+
   async onPairListDevices() {
     return [];
   }
