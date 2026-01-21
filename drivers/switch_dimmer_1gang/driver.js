bcbd68de3668030410cc2b6e65cf2c14c7ec372a
Attilla de Groot <attilla@nvidia.com>
2026-01-22 00:51:49 +0100

    next commit

--- drivers/switch_dimmer_1gang/driver.js
+++ drivers/switch_dimmer_1gang/driver.js
@@ -12,35 +12,13 @@ class SwitchDimmer1GangDriver extends Homey.Driver {
   }
 
   registerFlowCards() {
-    // Register trigger cards (already handled in device.js via trigger())
+    // Register trigger cards (these will be used in device.js via trigger())
     this.homey.flow.getDeviceTriggerCard('switch_dimmer_1gang_turned_on');
     this.homey.flow.getDeviceTriggerCard('switch_dimmer_1gang_turned_off');
     this.homey.flow.getDeviceTriggerCard('switch_dimmer_1gang_brightness_increased');
     this.homey.flow.getDeviceTriggerCard('switch_dimmer_1gang_brightness_decreased');
 
-    // Register condition card
-    this.homey.flow.getConditionCard('switch_dimmer_1gang_is_on')
-      .registerRunListener(async (args) => {
-        return args.device.getCapabilityValue('onoff');
-      });
-
-    // Register action cards
-    this.homey.flow.getActionCard('switch_dimmer_1gang_turn_on')
-      .registerRunListener(async (args) => {
-        return args.device.setCapabilityValue('onoff', true);
-      });
-
-    this.homey.flow.getActionCard('switch_dimmer_1gang_turn_off')
-      .registerRunListener(async (args) => {
-        return args.device.setCapabilityValue('onoff', false);
-      });
-
-    this.homey.flow.getActionCard('switch_dimmer_1gang_set_brightness')
-      .registerRunListener(async (args) => {
-        return args.device.setCapabilityValue('dim', args.brightness);
-      });
-
-    this.log('Flow cards registered');
+    this.log('Flow trigger cards registered');
   }
 
   async onPairListDevices() {
