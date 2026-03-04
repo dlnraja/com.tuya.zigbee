a58fb117800354e2be88f960c3b6d36f81a794dd
Dylan Rajasekaram <dylan.rajasekaram@gmail.com>
2026-03-04 17:59:35 +0100

    Enrich flow cards for 9 drivers: conditions, actions, registrations [skip ci]

--- drivers/lcdtemphumidsensor/driver.js
+++ drivers/lcdtemphumidsensor/driver.js
@@ -9,6 +9,12 @@ class LCDTempHumidSensorDriver extends ZigBeeDriver {
     this.log('  TS0201 with LCD display                                 ');
     this.log('═══════════════════════════════════════════════════════════');
     this._registeredIeeeAddresses = new Set();
+    this.homey.flow.getDeviceConditionCard('lcdtemphumidsensor_temp_above').registerRunListener(async (args) => {
+      return (args.device.getCapabilityValue('measure_temperature') || 0) > args.temperature;
+    });
+    this.homey.flow.getDeviceConditionCard('lcdtemphumidsensor_humidity_above').registerRunListener(async (args) => {
+      return (args.device.getCapabilityValue('measure_humidity') || 0) > args.humidity;
+    });
   }
 
   async onPairListDevices(devices) {
--- drivers/power_clamp_meter/driver.js
+++ drivers/power_clamp_meter/driver.js
@@ -5,6 +5,13 @@ const { Driver } = require('homey');
 class PowerClampMeterDriver extends Driver {
   async onInit() {
     this.log('Power Clamp Meter driver initialized');
+    this.powerChangedTrigger = this.homey.flow.getDeviceTriggerCard('power_clamp_meter_power_changed');
+    this.homey.flow.getDeviceConditionCard('power_clamp_meter_power_above').registerRunListener(async (args) => {
+      return (args.device.getCapabilityValue('measure_power') || 0) > args.power;
+    });
+    this.homey.flow.getDeviceActionCard('power_clamp_meter_reset_meter').registerRunListener(async (args) => {
+      await args.device.setCapabilityValue('meter_power', 0);
+    });
   }
 }
 
